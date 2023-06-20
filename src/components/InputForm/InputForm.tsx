e6838de5cda1a940b0186f7e5ba5618a4f928e15
Alicja Szulecka <73056579+AliSzu@users.noreply.github.com>
2023-06-20 12:54:40 +0200

    small fixes

--- src/components/InputForm/InputForm.tsx
+++ src/components/InputForm/InputForm.tsx
@@ -3,24 +3,28 @@ import classes from "./InputForm.module.scss";
 import { useTranslation, Trans } from "react-i18next";
 
 interface IInputForm {
-    onSubmit: (customName: string | undefined) => void
+  onSubmit: (customName: string | undefined) => void;
 }
 
-const InputForm = ({onSubmit} : IInputForm) => {
+const InputForm = ({ onSubmit }: IInputForm) => {
   const [customName, setCustomName] = useState<string | undefined>();
   const { t } = useTranslation();
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    onSubmit(customName)
+    onSubmit(customName);
   };
 
   const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const name = event.target.value
+    const name = event.target.value;
     setCustomName(name);
-
   };
   return (
-    <form className={classes.formControl} onSubmit={handleSubmit} noValidate={true}>
+    <form
+      className={classes.formControl}
+      onSubmit={handleSubmit}
+      noValidate={true}
+    >
       <input
         onChange={handleNameChange}
         required={true}
@@ -28,20 +32,19 @@ const InputForm = ({onSubmit} : IInputForm) => {
         id="ImpersonateInput"
       />
       <label className={classes.inputLabel} htmlFor="ImpersonateInput">
-        {t('inputField')}
+        {t("inputField")}
       </label>
 
-        <button
-          className={`btn btn--primary ${classes["btn--joke"]}`}
-          disabled={false}
-          aria-label="Draw Joke"
-          type="submit"
-        >
-          <Trans i18nKey='button' customName={customName}>
+      <button
+        className={`btn btn--primary ${classes["btn--joke"]}`}
+        disabled={false}
+        aria-label="Draw Joke"
+        type="submit"
+      >
+        <Trans i18nKey="button">
           Draw a random {customName ? customName : "Chuck Norris"} Joke
-          </Trans>
-          {/* TODO: I18 FOR TEXT AND TRANSLATIONS */}
-        </button>
+        </Trans>
+      </button>
     </form>
   );
 };
